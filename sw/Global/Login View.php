33db8d1b132db6f35122590d1756a5424c428906
Omar2710 <omar2011101@miuegypt.edu.eg>
2023-05-09 00:36:23 +0300

    signup using social media

---
+++ sw/Global/Login View.php
@@ -0,0 +1,29 @@
+<div class="txt1 text-center p-t-54 p-b-20">
+						<span>
+							Or Sign Up Using
+						</span>
+					</div>
+
+					<div class="flex-c-m">
+						<a href="#" class="login100-social-item bg1">
+							<i class="fa fa-facebook"></i>
+						</a>
+
+						<a href="#" class="login100-social-item bg2">
+							<i class="fa fa-twitter"></i>
+						</a>
+
+						<a href="#" class="login100-social-item bg3">
+							<i class="fa fa-google"></i>
+						</a>
+					</div>
+
+					<div class="flex-col-c p-t-155">
+						<span class="txt1 p-b-17">
+							Or Sign Up 
+						</span>
+
+						<a href="../App/Viewer/Register View.php" class="txt2">
+							Sign Up
+						</a>
+					</div>
