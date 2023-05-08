81ab1a2569552938d1c1c5751cc600e837fff1c4
HabebaMohamedd12 <habeba2007294@miuegypt.edu.eg>
2023-05-08 23:09:44 +0300

    Admin login

---
+++ sw/App/Model/Admin.php
@@ -0,0 +1,18 @@
+<?php
+
+class Admin{
+	private $Id;
+	private static $db;
+
+	public static function ConnectToDB(){
+		include_once "../../Model/Database2.php";
+        include_once "../../../Global/vars.php";
+        //$var = "../../../Global/vars.php";
+        $var = vars::getVars();
+		self::$db = new Database($var);
+		self::$db = self ::$db->db;
+	
+	}
+}
+
+?>
