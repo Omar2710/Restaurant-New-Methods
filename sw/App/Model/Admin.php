bca2cdf99c159dae19476af6a32fbb194b1599c4
HabebaMohamedd12 <habeba2007294@miuegypt.edu.eg>
2023-05-08 23:23:45 +0300

    remove comment

--- sw/App/Model/Admin.php
+++ sw/App/Model/Admin.php
@@ -7,7 +7,7 @@ class Admin{
 	public static function ConnectToDB(){
 		include_once "../../Model/Database2.php";
         include_once "../../../Global/vars.php";
-        //$var = "../../../Global/vars.php";
+        $var = "../../../Global/vars.php";
         $var = vars::getVars();
 		self::$db = new Database($var);
 		self::$db = self ::$db->db;
