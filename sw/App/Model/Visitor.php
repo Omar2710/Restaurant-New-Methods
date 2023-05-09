bbd168dfc0dd6a8801dec67997b59bd9121ebf2b
Omar2710 <omar2011101@miuegypt.edu.eg>
2023-05-09 20:29:30 +0300

    Visitor Create Account

---
+++ sw/App/Model/Visitor.php
@@ -0,0 +1,36 @@
+<?php
+include_once 'User.php';
+include_once 'Database2.php';
+include_once "../../Model/Database2.php";
+include_once "../../../Global/vars.php";
+
+class Visitor extends User{
+
+
+	private $order ;
+	public $dbo;
+
+
+ 
+	public function register($data){
+		// access and add to db
+		include_once "Database2.php";
+		include_once "../../Global/vars.php"; 
+        $var = vars::getVars();
+		$db = new Database($var);
+		$state = $db->add($data,'users');
+		return $state;
+
+	}	
+
+	public function ConnectToDB(){
+		include_once "../../Model/Database2.php";
+        include_once "../../../Global/vars.php";
+        $var = vars::getVars();
+		$this->dbo = new Database($var);
+		$this->dbo = $this->dbo->db;
+	
+	}   
+
+}
+?>
