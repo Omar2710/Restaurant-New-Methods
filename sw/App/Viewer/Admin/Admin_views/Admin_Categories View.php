cb163456e665724d0e4936d7ed2d5823811e6097
doniaSameh <donia2004436@miuegypt.edu.eg>
2023-05-16 19:54:54 +0300

    fixing search

--- sw/App/Viewer/Admin/Admin_views/Admin_Categories View.php
+++ sw/App/Viewer/Admin/Admin_views/Admin_Categories View.php
@@ -46,7 +46,15 @@
 ?>
         </table>
         
+      
+        <div class="container-login100-form-btn">
+                <div class="wrap-login100-form-btn">
+                        <div class="login100-form-bgbtn"></div>
+                        <button class="login100-form-btn" name="submit">
+                            <a style="color:#fff" href="Admin Manage Category.php?action=search">Search Category</a>
+                        </button>
 
+                </div>
                 <div class="wrap-login100-form-btn">
                         <div class="login100-form-bgbtn"></div>
                         <button class="login100-form-btn" name="submit">
