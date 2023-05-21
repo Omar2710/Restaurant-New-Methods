e67004c00b267294b170d25b1d043f751bb37c56
Omar2710 <omar2011101@miuegypt.edu.eg>
2023-05-21 19:23:26 +0300

    added function

--- sw/App/Model/OrderFoodItem.php
+++ sw/App/Model/OrderFoodItem.php
@@ -87,4 +87,17 @@ class OrderFoodItem{
    
 	}
 
+
+	public static function getAllItemOfOrder($orderId){
+		self::ConnectToDB2();
+    	$tablename = "orderfooditem";
+        $sql = "SELECT * from $tablename where orderID = $orderId";
+	    $stmt = self::$dbo2->prepare($sql);
+	    $stmt->execute();
+	    $rows=$stmt->fetchAll();
+	   	return $rows;
+	}
+
+
+
 }
