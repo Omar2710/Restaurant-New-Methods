75226da520d353443e15d9994c7793cce199b2c7
Omar2710 <omar2011101@miuegypt.edu.eg>
2023-05-21 17:16:02 +0300

    added functions

--- sw/App/Controller/Visitor/Visitor Manage Orders Controller.php
+++ sw/App/Controller/Visitor/Visitor Manage Orders Controller.php
@@ -19,4 +19,16 @@ if ($_POST OR @$_GET['action']) {
         include "../../Viewer/Visitor/Visitor_views/Visitor_subViews/Pending Orders.php";
     }
 
+    if (isset($_GET['action']) AND $_GET['action'] == "v_transactions") {
+        $orderData = Order::getDelieverdData($_SESSION['GroupID']); // gets all previous orders data
+        include "../../Viewer/Visitor/Visitor_views/Visitor_subViews/Transactions.php";
+   }
+
+
+    if (isset($_GET['action']) AND $_GET['action'] == "displayTransaction") {
+        $orderData = Order::getOrderData($_SESSION['GroupID'],$_GET['id'],$stateuse = 'delivered');
+        $orderItems = OrderFoodItem::getAllItemOfOrder($orderData[0]['ID']);
+        include "../../Viewer/Visitor/Visitor_views/Visitor_subViews/Display Order.php";
+       //print_r($orderData);
+   }
 }
