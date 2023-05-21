35b97f68d72e8d76f3bbef2a348adcc629ac66a7
Omar2710 <omar2011101@miuegypt.edu.eg>
2023-05-21 17:02:31 +0300

    transactions

---
+++ sw/App/Viewer/Visitor/Visitor_views/Visitor_Orders.php
@@ -0,0 +1,23 @@
+<div class="col-md-6">
+                            <div class="box">
+                                <div class="title">
+                                    <p><a href="Visitor Manage Orders Controller.php?action=v_transactions"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i>Previous Transactions</a></p>
+                            
+                                </div>
+                                    <div class="list">
+                                        <ul>
+                                            <li></li>
+                                            <li></li>
+                                            <li></li>
+                                            <li></li>
+                                            <li></li>
+                                        </ul>
+                                    </div>
+                                        <button><a href="Visitor Manage Orders Controller.php?action=v_transactions"><i class="fa fa-eye" aria-hidden="true"></i>Transactions</a></button>
+                            </div>   
+                        </div>
+                    </div>
+                <br></br>
+            </div>    
+    </div>
+</div>
