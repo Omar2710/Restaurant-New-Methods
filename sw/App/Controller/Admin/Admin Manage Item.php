c5aab03de893999f2bc36cab54e3d72be169d10e
Omar2710 <omar2011101@miuegypt.edu.eg>
2023-05-24 01:14:59 +0300

    updated12

--- sw/App/Controller/Admin/Admin Manage Item.php
+++ sw/App/Controller/Admin/Admin Manage Item.php
@@ -1,61 +1,206 @@
 <?php
+    $_SESSION['url'] = '../..';
+    $_SESSION['logout'] = '../../../Global/';
+    include '../../Tempelates/header.php';
+    include '../../Tempelates/navbar.php';     
 
-include_once "../../Model/Admin.php";
-include_once "../../Model/fooditem.php";
-include_once "../../Model/Category.php";
-session_start();
-$ID       = $_SESSION['GroupID'];
-$CategoryTable = "categories";
-if ($_POST OR @$_GET['action']) {
+?>
+<div class="limiter">
     
-    if (isset($_GET['action']) AND $_GET['action'] == "add item") {
-        $catdata = Category::listCategories();
-        $_SESSION['catdate'] = $catdata;   
-        include "../../Viewer/Admin/Admin_views/Admin_subViews/Add Food Item.php";        
-    }
+		<div class="container-login100">
+            <div class="wrap2-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+                    <h1 class="text-center">Update Item</h1>
 
-    
-    if (isset($_POST['submit']) && $_POST['submit'] == "Add") {
+                    <div class="container">
+                        
+                        <form class="form-horizontal" action="../../Controller/Admin/Admin Manage Item.php" method="post">
+                            <input type="hidden" name="ID" value="<?php echo $dataPro['ID']?>">
+                            <!-- start Name field -->
+                            
+                            <div class="wrap-input100 validate-input m-b-23" data-validate = "Name is reauired">
+                                <span class="label-input100"  name="username">Name</span>
+                                <input class="input100" required="required" type="text" name="name" pattern="[[A-Za-z].{2,}" title="Contains only characters not less than 3" placeholder="Type Name" value="<?php echo $dataPro['Name']?>">
+                                <span class="focus-input100" data-symbol="&#xf206;"></span>
+					       </div>
+                            <!-- End Name field -->
+                            <!-- start Amount field -->
+                            <div class="wrap-input100 validate-input " data-validate="Amount is required">
+				              <span class="label-input100">Amount</span>
+						      <input class="input100" required="required" type="text" name="amount" pattern="[1-9].{0,}" title="Must Be only Numbers " placeholder="Type Amount" value="<?php echo $dataPro['Amount'];?>">
+						      <span class="focus-input100" data-symbol="&#xf190;"></span>
+					       </div>
+                            
+                         
+                            <!-- End Amount field -->
+                            <!-- start Categories field -->
+                            <div class="form-group form-group-lg date">
+				              <span class="label-input100">Category Name</span>
+						      <select class="form-control disCat" required name="catid">
+                                        <option value="<?=$dataPro['CATID'];?>" ><?php echo $dataPro['Catigiories'];?></option>
+                                        
+                                    <?php
+            
+                                        foreach($catdata as $cat){
+                                            
+                                            echo "<option value='".$cat['ID']."'>".$cat['Name']."</option>";
+                                        }
+            
+                                    ?>
+                                </select>
+						      
+					       </div>
+                            
+                          
+                            <!-- End categories field -->
+                            <!-- start Price field -->
+                            <div class="wrap-input100 validate-input">
+				              <span class="label-input100">Price</span>
+						      <input class="input100" required="required" type="text"  name="price" placeholder="Price pre Unit" pattern="[1-9].{1,}" title="Must Be only Numbers not less than one" value="<?php echo $dataPro['Price']?>">
+						      
+					       </div>
+                            
+                           
+                            <!-- End Price field -->
 
-        $FoodItem = new fooditem($_POST);
-        $result = fooditem::SearchItemByName($FoodItem->Name); // checks if it exists or not
+                             <!-- start brithday field -->
+                                <div class="form-group form-group-lg">
+                                    <label class="col-sm-2 control-label">ProductDate</label>
+                                    <div class="col-sm-10 col-md-6 date">
+                                        <div class="day">
+                                            <select class="form-control" name="day1">
+                                           <?php
+                                                
+                                                for($i=01,$j=01;i<=31,$j<=31;$i++,$j++){
+                                                    
+                                                    
+                                                    echo "<option value='".$i."'";
+                                                    if($j == $day){echo ' selected';}
+                                                    echo   ">".$j."</option>";
+                                                }
+                                            ?>
+                                        </select>
+                                        </div>
+                                        <div class="month">
+                                            <select class="form-control" name="month1">
+                                           <?php
+                                                for($i=1,$j=1;i<=12,$j<=12;$i++,$j++){
+                                                   echo "<option value='".$i."'";
+                                                    if($j == $month){echo 'selected';}
+                                                    echo   ">".$j."</option>";
+                                                }
+                                            ?>
+                                        </select>
+                                        </div>
+                                        <div class="year">
+                                            <select class="form-control" name="year1">
+                                           <?php
+                                                for($i=2019,$j=2019;i<=1958,$j>=1958;$i--,$j--){
+                                                   echo "<option value='".$i."'";
+                                                    if($j == $year){echo 'selected';}
+                                                    echo   ">".$j."</option>";
+                                                }
+                                            ?>
+                                        </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- End brithday field -->
+                            
+                             <!-- start ExpireDate field -->
+                            <div class="form-group form-group-lg">
+                                    <label class="col-sm-2 control-label">ExpireDate</label>
+                                    <div class="col-sm-10 col-md-6 date">
+                                        <div class="day">
+                                            <select class="form-control" name="day2">
+                                           <?php
+                                                
+                                                for($i=01,$j=01;i<=31,$j<=31;$i++,$j++){
+                                                    
+                                                    
+                                                    echo "<option value='".$i."'";
+                                                    if($j == $exday){echo 'selected';}
+                                                    echo   ">".$j."</option>";
+                                                }
+                                            ?>
+                                        </select>
+                                        </div>
+                                        <div class="month">
+                                            <select class="form-control" name="month2">
+                                           <?php
+                                                for($i=1,$j=1;i<=12,$j<=12;$i++,$j++){
+                                                   echo "<option value='".$i."'";
+                                                    if($j == $exmonth){echo 'selected';}
+                                                    echo   ">".$j."</option>";
+                                                }
+                                            ?>
+                                        </select>
+                                        </div>
+                                        <div class="year">
+                                            <select class="form-control" name="year2">
+                                           <?php
+                                                for($i=2050,$j=2050;i<=2019,$j>=2019;$i--,$j--){
+                                                   echo "<option value='".$i."'";
+                                                    if($j == $exyear){echo 'selected';}
+                                                    echo   ">".$j."</option>";
+                                                }
+                                            ?>
+                                        </select>
+                                        </div>
+                                    </div>
+                                </div>
+                            <!-- End ExpireDate field -->
+                            
+                            <!-- start productimg field -->
+                        
+                            <div class="wrap-input100 validate-input">
+				              <span class="label-input100 ">Image</span>
+						      <input class="input100" type="image" name="productimg" value="<?php echo $dataPro['Image']?>">
+						      
+					       </div>
+                            <!-- End productimg field -->
 
-        if($result == 1){ // if that food item exist it will increase its amount only
+                             <!-- start Description field -->
+                            
+                             <div class="wrap-input100 validate-input">
+				              <span class="label-input100">Description</span>
+						      <input class="input100" required="required" type="text"  name="desc" placeholder="Type Description" value="<?php echo $dataPro['Description']?>">
+						      
+					       </div>
+                            <!-- End Description field -->                             
+                            <div>
+                                <span>Visibility :</span>
+                                    
+                                    <?php if ($dataPro['Visibility']==1) {
+                                                echo "<input type='radio' name='vis'  checked value = 1> Enable" ;
+                                                echo "<input type='radio' name='vis' value = 0 > Disabel" ;
+                                            }
+                                            else{
+                                                echo "<input type='radio' name='vis' value = 1> Enable";
+                                                echo "<input type='radio' name='vis'  checked value = > Disabel" ;
+                                            }
 
-            $currentAmount = fooditem::getItemAmount($FoodItem->Name);
-            $currentAmount['Amount'] += $FoodItem->Amount;
-            fooditem::setItemAmount($currentAmount['Amount'],$FoodItem->Name);
-           //  we need to notify that item is alredy exists
-            header('Location:../../Controller/Admin/Admin Dashboard Controller.php?action=v_FoodItems');
-        }else{
-           
-            $main['Name'] = $FoodItem->Name; 
-            $main['Amount'] = $FoodItem->Amount; 
-            $main['CATID'] = $FoodItem->CATID; 
-            $main['Price'] = $FoodItem->Price;
-            $date  =$_POST['year'];
-            $date .='-'.$_POST['month'];
-            $date .='-'.$_POST['day'];
-            $exdate  =$_POST['exyear'];
-            $exdate .='-'.$_POST['exmonth'];
-            $exdate .='-'.$_POST['exday'];
-            $main['Visibility'] = $FoodItem->Visibility;
-            $main['ExpireDate'] = $exdate;
-            $main['ProductDate'] = $date ;
-            $main['Description'] = $FoodItem->Description;
-            //print_r($main);
-            try {
-                
-                $dd = $FoodItem->AddNewItem ($main);
-                if($dd == true){
-                    header('Location:../../Controller/Admin/Admin Dashboard Controller.php?action=v_FoodItems');
-                }
-                
+                                    ?>
 
-            } catch (Exception $exc) {
-                echo $exc->getMessage();
-            } 
-       }
-        
-    }
-}
+                                
+                            </div>
+                            <br>
+                            <div class="container-login100-form-btn">
+                              <div class="wrap-login100-form-btn">
+                                <div class="login100-form-bgbtn"></div>
+                                <button class="login100-form-btn" name="submit">
+                                    <input class="inpt" name="submit"   value="Edit" >
+                                </button>
+
+                                </div>
+					       </div>
+                            
+                            
+                        </form>
+                        
+            </div>
+            </div>
+    </div>
+</div>
+    <?php include '../../Tempelates/footer.php';
+
+?>
